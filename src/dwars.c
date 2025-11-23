a6a8ab5815578bddbd2f4b5038f84704c01a09d0
Lucas <santoslucas347@gmail.com>
2025-11-23 20:33:07 -0300

    Screen dynamic scaling,pillarbox and letterbox

--- src/dwars.c
+++ src/dwars.c
@@ -2,31 +2,78 @@
 #include "scene.h"
 #include "assets.h"
 
-static int gameWidth = 1280;
+static int gameWidth  = 1280;
 static int gameHeight = 720;
+static float screenScale = 0;
+static RenderTexture2D framebufferRender;
 
 scene CurrentScene = GAMEPLAY; 
+
+
+void UpdateFrame();
+void DrawFrame();
 
 int main(){
 
     SetTargetFPS(30);
-    InitWindow(gameWidth,gameHeight,"War of Donuts");
-	
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(gameWidth,gameHeight,"War of Donuts");
+	framebufferRender = LoadRenderTexture(gameWidth,gameHeight);
+	SetTextureFilter(framebufferRender.texture,TEXTURE_FILTER_BILINEAR);
+
 	LoadSprites(); //TODO: check for errors
 
     while(!WindowShouldClose()){
     	 
-		BeginDrawing();
-			DrawGameplayScene();	
-            ClearBackground(PURPLE);
-		//	DrawTexture(jetText,GetMouseX()-128,GetMouseY()-128,WHITE);	
-				
-        EndDrawing();
+		float widthScale  = (float)GetScreenWidth() / gameWidth;
+		float heightScale = (float)GetScreenHeight() / gameHeight;
+		screenScale = (widthScale < heightScale) ? widthScale : heightScale; 
+		//screenScale = heightScale;		
 
+		UpdateFrame();
+		DrawFrame();
 
     }
 	//TODO: Unloading
+	UnloadRenderTexture(framebufferRender);
     CloseWindow();
 
     return 0;
+}
+
+void UpdateFrame(){
+
+}
+
+void DrawFrame(){
+	BeginTextureMode(framebufferRender);
+
+		DrawGameplayScene();
+		for(int y = 0; y < 10;y++){
+			DrawRectangle(0,y*100+100,1280,20,RED);
+		}		
+
+	EndTextureMode();	
+	
+	BeginDrawing();
+		
+		ClearBackground(BLACK);
+
+		Rectangle sourceRect = {
+			0.0f,
+			0.0f,
+			framebufferRender.texture.width,
+			-framebufferRender.texture.height};
+
+		Rectangle destRect = {
+			(float)(GetScreenWidth()  - (float)screenScale  * gameWidth)/2.0,
+			(float)(GetScreenHeight() - (float)screenScale  * gameHeight)/2.0,
+			(float) gameWidth  * screenScale,
+			(float) gameHeight * screenScale};
+
+		DrawTexturePro(framebufferRender.texture,sourceRect,destRect,(Vector2) {0.0f,0.0f},0.0f,WHITE);
+
+	EndDrawing();
+	
+
 }
